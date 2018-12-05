fe8fe57dc6ef133bb503d9cb23cccc20a885e3bc
TySanteOn <dante.verbauwhede@gmail.com>
2018-12-05 20:01:01 +0100

    webpack added

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Neverbland_frontendChallenge
+# Neverbland front-end challenge
+## Neverblands challenge for a possible internship
+
+### possible features:
+
+* data​ ​from​ ​the​ ​​TV​ ​Maze​ ​API​.
+* The​ ​layout​ ​should​ ​be​ ​based​ ​on​ ​the​ ​provided​ ​wireframes,​ ​however​ ​the​ ​look,​ ​feel​ ​and​ ​any interactivity​ ​is​ ​up​ ​to​ ​you​ ​-​ ​don’t​ ​be​ ​afraid​ ​to​ ​get​ ​creative!
+* Work​ ​should​ ​be​ ​submitted​ ​as​ ​a​ ​Git​ ​repository.
+* The site should be built using HTML, CSS and JavaScript, and as far as styling is concerned:
+SCSS,​ ​Pre/Post​ ​CSS,​ ​CSS​ ​Modules,​ ​and​ ​CSS-in-JS​ ​are​ ​all​ ​encouraged.
+* The​ ​site​ ​should​ ​be​ ​built​ ​using​ ​responsive​ ​techniques​ ​to​ ​behave​ ​well​ ​on​ ​a​ ​variety​ ​of​ ​devices.
+* The​ ​site​ ​should​ ​function​ ​as​ ​a​ ​single​ ​​page​ ​application.
+* The​ ​site​ ​should​ ​support​ ​the​ ​last​ ​2​ ​versions​ ​of​ ​Chrome,​ ​Firefox,​ ​Safari,​ ​Edge​ ​and​ ​Internet​ ​Explorer.
+* We​ ​encourage​ ​the​ ​use​ ​of​ ​a​ ​framework​ ​-​ ​our​ ​preference​ ​is​ ​React.
+
+
+### possible extra features
+
+* The​ ​use​ ​of​ ​a​ ​build​ ​tool
+* Responsive​ ​images
+* Tests​ ​(Unit​ ​and/or​ ​end-to-end)
+* Pagination
+* Performance​ ​optimisation
---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require(`path`);
+const HtmlWebpackPlugin = require(`html-webpack-plugin`);
+
+const merge = require(`webpack-merge`);
+const parts = require(`./webpack.parts.js`);
+
+const PATHS = {
+  src: path.join(__dirname, `src`),
+  dist: path.join(__dirname, `dist`)
+};
+
+const commonConfig = merge([ {
+  entry: [
+    path.join(PATHS.src, `css/style.css`),
+    path.join(PATHS.src, `js/script.jsx`)
+  ],
+  output: {
+    path: PATHS.dist,
+    filename: `js/script.[hash].js`
+  },
+  module: {
+    rules: [{
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        loader: `babel-loader`,
+      },
+      {
+        test: /\.html$/,
+        loader: `html-loader`,
+      },
+      {
+        test: /\.jpe?g$/,
+        loader: `url-loader`,
+        options: {
+          limit: 1000,
+          context: `./src`,
+          name: `[path][name].[ext]`
+        },
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: `./src/index.html`
+    })
+  ]
+}]);
+
+const productionConfig = merge([
+  parts.extractCSS()
+]);
+
+const developmentConfig = merge([
+  {
+    devServer: {
+      overlay: true,
+      contentBase: PATHS.src
+    }
+  },
+  parts.loadCSS()
+]);
+
+module.exports = env => {
+  if (process.env.NODE_ENV === 'production') {
+    console.log('building production');
+    return merge(commonConfig, productionConfig);
+  }
+  return merge(commonConfig, developmentConfig);
+};
+  
